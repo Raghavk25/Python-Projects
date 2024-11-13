2fd9a4e0884e4aa5e322bb433ae4f70e499491b0
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-11-13 20:14:24 +0530

    Update main.py

--- Secret auction/main.py
+++ Secret auction/main.py
@@ -1,4 +1,8 @@
-#Secret Auction Program
+#Secret auction program
+
+# MIT License
+# Copyright (c) 2024 Raghav Khanna
+# See the LICENSE file in the root of the repository for full license details.
 
 from replit import clear
 from art import *
