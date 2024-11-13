186d8065a5cbaef3c07f2e0d8a3dba80bd0fb61b
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-11-13 20:10:23 +0530

    Update main.py

--- German-English flash cards game/main.py
+++ German-English flash cards game/main.py
@@ -1,5 +1,9 @@
 #German to English flash cards
 
+# MIT License
+# Copyright (c) 2024 Raghav Khanna
+# See the LICENSE file in the root of the repository for full license details.
+
 from tkinter import *
 import random
 import pandas
