aeacd2988610808f79a40f06c0de80cfb88b83d7
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-11-13 21:49:25 +0530

    Update main.py

--- Stock market news updates/main.py
+++ Stock market news updates/main.py
@@ -1,3 +1,9 @@
+# Stock market news updates
+
+# MIT License
+# Copyright (c) 2024 Raghav Khanna
+# See the LICENSE file in the root of the repository for full license details.
+
 import requests
 import smtplib
 
