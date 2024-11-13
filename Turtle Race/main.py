fbf74e80f43867c7501abcf5cdf57d0975872ae8
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-11-13 20:17:13 +0530

    Update main.py

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -1,5 +1,9 @@
 #Turtle Race
 
+# MIT License
+# Copyright (c) 2024 Raghav Khanna
+# See the LICENSE file in the root of the repository for full license details.
+
 import turtle
 import random
 import time
