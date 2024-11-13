2a9d9b346c036d984df343868261898fe9ceb11a
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-11-13 20:16:56 +0530

    Update ui.py

--- Trivia quiz/ui.py
+++ Trivia quiz/ui.py
@@ -1,3 +1,7 @@
+# MIT License
+# Copyright (c) 2024 Raghav Khanna
+# See the LICENSE file in the root of the repository for full license details.
+
 from tkinter import *
 from quiz_brain import QuizBrain
 
