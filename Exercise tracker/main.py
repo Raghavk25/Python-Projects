fbcee8019eb4a746dd7bde1a0a792e56f2c30e60
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-12-25 00:29:41 +0530

    Update main.py

--- Exercise tracker/main.py
+++ Exercise tracker/main.py
@@ -1,9 +1,5 @@
 # Exercise tracker
 
-# MIT License
-# Copyright (c) 2024 Raghav Khanna
-# See the LICENSE file in the root of the repository for full license details.
-
 import requests
 from datetime import datetime
 
