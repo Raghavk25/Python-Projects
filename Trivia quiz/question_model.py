2d8ce2371a977bc04dea2395cffe38c0b4a64255
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-12-25 00:37:59 +0530

    Update question_model.py

--- Trivia quiz/question_model.py
+++ Trivia quiz/question_model.py
@@ -1,7 +1,3 @@
-# MIT License
-# Copyright (c) 2024 Raghav Khanna
-# See the LICENSE file in the root of the repository for full license details.
-
 class Question:
 
     def __init__(self, q_text, q_answer):
